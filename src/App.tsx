b81f33fc9fcb2a9e9326c3845328feff1a6cbae7
Jesse Hoogland <jessequinten@gmail.com>
2022-07-25 16:46:34 +0200

    ADD button to add new items (named 'Todo #0', 'Todo #1', etc.)

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
+import { useState } from "react"
+
+interface TodoItem {
+  id: number;
+  value: string
+}
 
 export default function App() {
+  const [todos, setTodos] = useState<TodoItem[]>([])
+
+  const handleCreateItem = () => {
+    setTodos([...todos, { id: todos.length, value: `Todo #${todos.length}` }])
+  }
+
   return (
     <main>
-      Hello World
+      <ul>
+        {todos.map(todo => (
+          <li key={todo.id}>{todo.value}</li>
+        ))} 
+        </ul>
+      <button onClick={handleCreateItem}>Add Item</button>
     </main>
   )
 }
