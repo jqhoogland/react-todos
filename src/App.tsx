6cb2324e639819b2d6c1a95cd55071eb085a41e3
Jesse Hoogland <jessequinten@gmail.com>
2022-07-25 16:07:55 +0200

    RM everything (back to bare package)

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,8 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { Layout } from "./components/layout";
-import ThemeProvider from './components/ThemeProvider';
-import Tasks from "./pages/Tasks";
-import Users from './pages/Users';
 
 export default function App() {
   return (
-    <ThemeProvider>
-      <Layout>
-        <Routes>
-          <Route path="" element={<Tasks />}/>
-          <Route path="/users" element={<Users />}/>
-        </Routes>
-      </Layout>
-    </ThemeProvider>
+    <main>
+      Hello World
+    </main>
   )
 }
