2e1905297111daf8f4b27c364a6146071af34c44
Jesse Hoogland <jessequinten@gmail.com>
2022-07-26 15:16:07 +0200

    memoize the hooks in hooks.ts

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
 export function usePersistedState<T>(key: string, initialValue: T) {
     // Can also load this in a `useLayoutEffect` instead
@@ -9,26 +9,26 @@ export function usePersistedState<T>(key: string, initialValue: T) {
     
     // Can also `localStorage.setItem` in a `useEffect` that listens to `value`, 
     // but this is not the "proper" way to do it.
-    const setAndSaveValue = (newValue: T) => {
+    const setAndSaveValue = useCallback((newValue: T) => {
         localStorage.setItem(key, JSON.stringify(newValue));
         setValue(newValue);
-    }
+    }, [setValue])
   
     return [value, setAndSaveValue] as const;
 }
   
 export function useListMethods<T extends  {id: number }>(list: T[], setList: (list: T[]) => void) {
-  const create = (item: Omit<T, 'id'>) => {
+  const create = useCallback((item: Omit<T, 'id'>) => {
     setList([...list, { id: list.length, ...item } as T])
-  }
+  }, [setList])
 
-  const update = (id: number, newValue: Partial<T>) => {
+  const update = useCallback((id: number, newValue: Partial<T>) => {
     setList(list.map(item => item.id === id ? { ...item, ...newValue } : item))
-  }
+  }, [setList])
 
-  const remove = (id: number) => {
+  const remove = useCallback((id: number) => {
     setList(list.filter(item => item.id !== id))
-  }
+  }, [setList])
 
   return { create, update, remove }
 }
